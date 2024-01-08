1627b6ebd5b058164dd034b72b838febb5ec434d
m k laxmi <maradimanil@gmail.com>
2024-01-08 16:31:34 +0530

    Added eureka and api gateway

--- inventory-service/src/main/java/com/springboot/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/springboot/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.springboot.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/springboot/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/springboot/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -32,7 +32,7 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get().uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
         // if all requested products are in stock then return TRUE else FALSE
         boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
 
